be0158f4105914d4308d7d61e052c22155589f77
Randy <randy408@protonmail.com>
2020-08-28 21:54:53 +0200

    Revert "simplify ecs.struct/array() cfunctions"
    This reverts commit 5940d1d259a0f09e9a758a61d60137389b1dd1cc.

--- src/ecs.c
+++ src/ecs.c
@@ -220,17 +220,17 @@ static int new_array(lua_State *L)
     const char *name = luaL_checkstring(L, 1);
     const char *desc = luaL_checkstring(L, 2);
 
-    ecs_entity_t e = ecs_new(w, 0);
+    ecs_entity_t ecs_entity(EcsMetaType) = ecs_lookup_fullpath(w, "flecs.meta.MetaType");
 
-    EcsMetaType meta =
+    ecs_entity_t e = 0;
+
+    e = ecs_set(w, 0, EcsMetaType,
     {
         .kind = EcsArrayType,
         .size = 0,
         .alignment = 0,
         .descriptor = desc
-    };
-
-    ecs_new_meta(w, e, &meta);
+    });
 
     ecs_set(w, e, EcsName, {.alloc_value = (char*)name});
 
@@ -246,17 +246,17 @@ static int new_struct(lua_State *L)
     const char *name = luaL_checkstring(L, 1);
     const char *desc = luaL_checkstring(L, 2);
 
-    ecs_entity_t e = ecs_new(w, 0);
+    ecs_entity_t ecs_entity(EcsMetaType) = ecs_lookup_fullpath(w, "flecs.meta.MetaType");
 
-    EcsMetaType meta =
+    ecs_entity_t e = 0;
+
+    e = ecs_set(w, 0, EcsMetaType,
     {
         .kind = EcsStructType,
         .size = 0,
         .alignment = 0,
         .descriptor = desc
-    };
-
-    ecs_new_meta(w, e, &meta);
+    });
 
     ecs_set(w, e, EcsName, {.alloc_value = (char*)name});
 
