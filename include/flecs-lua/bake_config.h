e117fc17e9f6820fb85afc1c1dc8e14ee46b0388
Randy <randy408@protonmail.com>
2020-11-09 00:38:11 +0100

    FLECS_LUA_EXPORT -> FLECS_LUA_API

--- include/flecs-lua/bake_config.h
+++ include/flecs-lua/bake_config.h
@@ -25,16 +25,16 @@
 /* Convenience macro for exporting symbols */
 #ifndef flecs_lua_STATIC
 #if flecs_lua_EXPORTS && (defined(_MSC_VER) || defined(__MINGW32__))
-  #define FLECS_LUA_EXPORT __declspec(dllexport)
+  #define FLECS_LUA_API __declspec(dllexport)
 #elif flecs_lua_EXPORTS
-  #define FLECS_LUA_EXPORT __attribute__((__visibility__("default")))
+  #define FLECS_LUA_API __attribute__((__visibility__("default")))
 #elif defined _MSC_VER
-  #define FLECS_LUA_EXPORT __declspec(dllimport)
+  #define FLECS_LUA_API __declspec(dllimport)
 #else
-  #define FLECS_LUA_EXPORT
+  #define FLECS_LUA_API
 #endif
 #else
-  #define FLECS_LUA_EXPORT
+  #define FLECS_LUA_API
 #endif
 
 #endif
--- include/flecs_lua.h
+++ include/flecs_lua.h
@@ -20,23 +20,23 @@ typedef struct FlecsLua
     ECS_DECLARE_COMPONENT(EcsLuaHost);
 }FlecsLua;
 
-FLECS_LUA_EXPORT
+FLECS_LUA_API
 void FlecsLuaImport(ecs_world_t *w);
 
 #define FlecsLuaImportHandles(handles)\
     ECS_IMPORT_COMPONENT(handles, EcsLuaHost);\
 
 /* Reinitialize with a custom lua_State */
-FLECS_LUA_EXPORT
+FLECS_LUA_API
 int ecs_lua_set_state(ecs_world_t *w, lua_State *L);
 
 /* Call progress function callback (if set),
    this is meant to be called between iterations. */
-FLECS_LUA_EXPORT
+FLECS_LUA_API
 void ecs_lua_progress(lua_State *L);
 
 /* Pushes the component at ptr onto the stack */
-FLECS_LUA_EXPORT
+FLECS_LUA_API
 void ecs_ptr_to_lua(
     ecs_world_t *world,
     lua_State *L,
@@ -44,7 +44,7 @@ void ecs_ptr_to_lua(
     const void *ptr);
 
 /* Converts the Lua value at the given index to the component type */
-FLECS_LUA_EXPORT
+FLECS_LUA_API
 void ecs_lua_to_ptr(
     ecs_world_t *world,
     lua_State *L,
@@ -53,21 +53,21 @@ void ecs_lua_to_ptr(
     void *ptr);
 
 /* Pushes the iterator onto the stack */
-FLECS_LUA_EXPORT
+FLECS_LUA_API
 void ecs_iter_to_lua(ecs_iter_t *it, lua_State *L, ecs_type_t select, bool copy);
 
 /* Converts the columns of the iterator at the given index */
-FLECS_LUA_EXPORT
+FLECS_LUA_API
 void ecs_lua_to_iter(ecs_world_t *world, lua_State *L, int idx);
 
 /* Create an EmmyLua class annotation */
 char *ecs_type_to_emmylua(ecs_world_t *world, ecs_entity_t type, bool struct_as_table);
 
 /* Get world pointer from registry */
-FLECS_LUA_EXPORT
+FLECS_LUA_API
 ecs_world_t *ecs_lua_get_world(lua_State *L);
 
-FLECS_LUA_EXPORT
+FLECS_LUA_API
 int luaopen_ecs(lua_State *L);
 
 #ifdef __cplusplus
