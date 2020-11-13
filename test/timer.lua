4374c01e7a795a71db2fe478c8f04c8cd68e676d
Randy <randy408@protonmail.com>
2020-11-13 17:59:31 +0100

    add test/timer.lua

---
+++ test/timer.lua
@@ -0,0 +1,59 @@
+local ecs = require "ecs"
+local t = require "test"
+local u = require "util"
+
+ecs.progress_cb(function() ecs.log("progress()!") end)
+
+
+local system_a_invoked = false
+local system_b_invoked = false
+local system_c_invoked = false
+
+
+local function SystemA(it)
+    ecs.log("time: ", it.delta_time)
+    --assert(it.delta_time == 1.0)
+    --assert(it.delta_system_time == 3.0)
+    system_a_invoked = true
+end
+
+local function SystemB(it)
+    assert(it.delta_time == 1)
+    assert(it.delta_system_time == 3)
+    system_b_invoked = true
+end
+
+local function SystemC(it)
+    assert(it.delta_time == 1.0)
+    assert(it.delta_system_time == 6.0)
+    system_b_invoked = true
+end
+
+local Position = ecs.struct("Position", "{float x; float y;}")
+ecs.new(nil, "Position")
+
+local system_a = ecs.system(SystemA, "SystemA", ecs.OnUpdate, Position)
+
+local filter = ecs.set_rate_filter(system_a, 3, 0)
+assert(filter ~= 0)
+assert(filter == system_a)
+
+
+assert(system_a_invoked == false)
+ecs.progress(1.0)
+assert(system_a_invoked == false)
+ecs.progress(1.0)
+assert(system_a_invoked == false)
+
+ecs.progress(1.0)
+assert(system_a_invoked == true)
+
+system_a_invoked = false
+
+ecs.progress(1.0)
+assert(system_a_invoked == false)
+ecs.progress(1.0)
+assert(system_a_invoked == false)
+ecs.progress(1.0)
+assert(system_a_invoked == true)
+
